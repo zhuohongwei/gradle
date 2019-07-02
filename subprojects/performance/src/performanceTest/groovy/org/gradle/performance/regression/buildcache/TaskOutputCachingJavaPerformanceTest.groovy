6459f793bca4ae81b87aba1349603b6319e6736a
Bo Zhang <bo@gradle.com>
2019-07-02 18:52:23 +0800

    Add GC args to performance test

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/TaskOutputCachingJavaPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/TaskOutputCachingJavaPerformanceTest.groovy
@@ -148,6 +148,13 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
         if (parallel) {
             runner.args += "--parallel"
         }
+
+        File logDir = new File(temporaryFolder.testDirectory, "${System.currentTimeMillis()}")
+        logDir.mkdirs()
+        File log = new File(logDir, "log.txt")
+
+        runner.gradleOpts = ["-Xloggc:${log.absolutePath}", '-XX:+PrintGCDateStamps', '-XX:+UnlockDiagnosticVMOptions', '-XX:+PrintFlagsFinal']
+
         pushToRemote = false
         runner.addBuildExperimentListener(touchCacheArtifacts())
 
@@ -156,6 +163,9 @@ class TaskOutputCachingJavaPerformanceTest extends AbstractTaskOutputCachingPerf
 
         then:
         result.assertCurrentVersionHasNotRegressed()
+
+        cleanup:
+        println log.text
 
         where:
         testScenario << [scenarios, [true, false]].combinations()
