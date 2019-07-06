67e9533af93fa006fbf8194f7ed364d03233820a
Sterling Greene <sterling@gradle.com>
2019-07-06 01:57:09 -0400

    Revert "Improve HttpClientHelper deprication error and strip user credentials in errors"
    This reverts commit e493d1960aef76d1def664cfd348bb5c1033886d.

--- subprojects/resources-http/src/main/java/org/gradle/internal/resource/transport/http/HttpClientHelper.java
+++ subprojects/resources-http/src/main/java/org/gradle/internal/resource/transport/http/HttpClientHelper.java
@@ -16,31 +16,25 @@
 
 package org.gradle.internal.resource.transport.http;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Iterables;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
-import org.gradle.internal.UncheckedException;
 import org.gradle.util.DeprecationLogger;
 import org.gradle.util.GUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -98,9 +92,9 @@ public class HttpClientHelper implements Closeable {
         try {
             return executeGetOrHead(request);
         } catch (FailureFromRedirectLocation e) {
-            throw new HttpRequestException(String.format("Could not %s '%s'.", method, stripUserCredentials(e.getLastRedirectLocation())), e.getCause());
+            throw new HttpRequestException(String.format("Could not %s '%s'.", method, e.getLastRedirectLocation()), e.getCause());
         } catch (IOException e) {
-            throw new HttpRequestException(String.format("Could not %s '%s'.", method, stripUserCredentials(request.getURI())), e);
+            throw new HttpRequestException(String.format("Could not %s '%s'.", method, request.getURI()), e);
         }
     }
 
@@ -137,14 +131,14 @@ public class HttpClientHelper implements Closeable {
     private HttpClientResponse performHttpRequest(HttpRequestBase request, HttpContext httpContext) throws IOException {
         // Without this, HTTP Client prohibits multiple redirects to the same location within the same context
         httpContext.removeAttribute(REDIRECT_LOCATIONS);
-        LOGGER.debug("Performing HTTP {}: {}", request.getMethod(), stripUserCredentials(request.getURI()));
+        LOGGER.debug("Performing HTTP {}: {}", request.getMethod(), request.getURI());
         validateUrl(request.getURI());
         try {
             CloseableHttpResponse response = getClient().execute(request, httpContext);
             return toHttpClientResponse(request, httpContext, response);
         } catch (IOException e) {
             validateRedirectChain(httpContext);
-            URI lastRedirectLocation = stripUserCredentials(getLastRedirectLocation(httpContext));
+            URI lastRedirectLocation = getLastRedirectLocation(httpContext);
             throw (lastRedirectLocation == null) ? e : new FailureFromRedirectLocation(lastRedirectLocation, e);
         }
     }
@@ -164,11 +158,12 @@ public class HttpClientHelper implements Closeable {
         if (settings.allowInsecureProtocol()) {
             return;
         }
+
         if (!GUtil.isSecureUrl(url)) {
             DeprecationLogger.nagUserWithDeprecatedIndirectUserCodeCause(
                 "Insecure HTTP requests",
                 "Switch the protocol to HTTPS or allow insecure protocols.",
-                "The URL was '" + stripUserCredentials(url) + "'."
+                "The URL was '" + url + "'."
             );
         }
     }
@@ -198,11 +193,11 @@ public class HttpClientHelper implements Closeable {
 
     private HttpClientResponse processResponse(HttpClientResponse response) {
         if (response.wasMissing()) {
-            LOGGER.info("Resource missing. [HTTP {}: {}]", response.getMethod(), stripUserCredentials(response.getEffectiveUri()));
+            LOGGER.info("Resource missing. [HTTP {}: {}]", response.getMethod(), response.getEffectiveUri());
             return null;
         }
         if (!response.wasSuccessful()) {
-            URI effectiveUri = stripUserCredentials(response.getEffectiveUri());
+            URI effectiveUri = response.getEffectiveUri();
             LOGGER.info("Failed to get resource: {}. [HTTP {}: {})]", response.getMethod(), response.getStatusLine(), effectiveUri);
             throw new HttpErrorStatusCodeException(response.getMethod(), effectiveUri.toString(), response.getStatusLine().getStatusCode(), response.getStatusLine().getReasonPhrase());
         }
@@ -226,23 +221,6 @@ public class HttpClientHelper implements Closeable {
             if (sharedContext != null) {
                 sharedContext.clear();
             }
-        }
-    }
-
-    /**
-     * Strips the {@link URI#getUserInfo() user info} from the {@link URI} making it
-     * safe to appear in log messages.
-     */
-    @Nullable
-    @VisibleForTesting
-    static URI stripUserCredentials(@CheckForNull URI uri) {
-        if (uri == null) {
-            return null;
-        }
-        try {
-            return new URIBuilder(uri).setUserInfo(null).build();
-        } catch (URISyntaxException e) {
-            throw UncheckedException.throwAsUncheckedException(e, true);
         }
     }
 
