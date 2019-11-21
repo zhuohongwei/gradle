69133a02efd5f62daa13381390c3475ea1250f7d
Bo Zhang <bo@gradle.com>
2019-11-21 09:44:55 +0800

    Disable build cache on windows

--- .teamcity/common/BuildCache.kt
+++ .teamcity/common/BuildCache.kt
@@ -22,10 +22,10 @@ interface BuildCache {
 
 data class RemoteBuildCache(val url: String, val username: String = "%gradle.cache.remote.username%", val password: String = "%gradle.cache.remote.password%") : BuildCache {
     override fun gradleParameters(os: Os): List<String> {
-        return listOf("--build-cache",
-                os.escapeKeyValuePair("-Dgradle.cache.remote.url", url),
-                os.escapeKeyValuePair("-Dgradle.cache.remote.username", username),
-                os.escapeKeyValuePair("-Dgradle.cache.remote.password", password)
+        return if (os == Os.windows) emptyList() else listOf("--build-cache",
+            os.escapeKeyValuePair("-Dgradle.cache.remote.url", url),
+            os.escapeKeyValuePair("-Dgradle.cache.remote.username", username),
+            os.escapeKeyValuePair("-Dgradle.cache.remote.password", password)
         )
     }
 }
