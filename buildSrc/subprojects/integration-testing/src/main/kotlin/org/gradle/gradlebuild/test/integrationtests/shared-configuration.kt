03d88f2fe9f438bc4dda6d07d2c98dc47e335353
Bo Zhang <bo@gradle.com>
2019-12-15 19:31:49 +0800

    fuck

--- buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/integrationtests/shared-configuration.kt
+++ buildSrc/subprojects/integration-testing/src/main/kotlin/org/gradle/gradlebuild/test/integrationtests/shared-configuration.kt
@@ -59,6 +59,7 @@ fun Project.createTasks(sourceSet: SourceSet, testType: TestType) {
     // For all of the other executers, add an executer specific task
     testType.executers.forEach { executer ->
         val taskName = "$executer${prefix.capitalize()}Test"
+        println("fuck3")
         createTestTask(taskName, executer, sourceSet, testType, Action {
             if (testType == TestType.CROSSVERSION) {
                 // the main crossVersion test tasks always only check the latest version,
@@ -81,8 +82,11 @@ fun Project.createTasks(sourceSet: SourceSet, testType: TestType) {
 
 
 internal
-fun Project.createTestTask(name: String, executer: String, sourceSet: SourceSet, testType: TestType, extraConfig: Action<IntegrationTest>): TaskProvider<IntegrationTest> =
+fun Project.createTestTask(name: String, executer: String, sourceSet: SourceSet, testType: TestType, extraConfig: Action<IntegrationTest>): TaskProvider<IntegrationTest> {
+    println("fuck2")
+
     tasks.register(name, IntegrationTest::class) {
+        println("fuck1")
         BuildBucketProvider.getInstance(project).configureTest(this, sourceSet, testType)
         description = "Runs ${testType.prefix} with $executer executer"
         systemProperties["org.gradle.integtest.executer"] = executer
@@ -92,6 +96,7 @@ fun Project.createTestTask(name: String, executer: String, sourceSet: SourceSet,
         libsRepository.required = testType.libRepoRequired
         extraConfig.execute(this)
     }
+}
 
 
 fun splitIntoBuckets(sourceFiles: List<File>, numberOfSplits: Int): List<List<File>> =
